75959bb473e800541931c974d1d0e7e862a263b7
russab <rusua967@gmail.com>
2020-03-21 17:35:08 +0300

    minor fix

--- cmd/facade/main.go
+++ cmd/facade/main.go
@@ -6,14 +6,11 @@ import (
 )
 
 func main()	{
-
 	in := "Read books\nListen what your mentor tells you\n" +
 				"Do the practice tasks given by mentors\n"
 
 	wbTrial := facade.NewTrial()
-
 	out := wbTrial.Plan()
-
 	if in != out	{
 		fmt.Printf("Expected: %s\nGot: %s\n", in, out)
 	}
